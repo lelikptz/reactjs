1d8efee7e8695e35447aa2c58a9d15e2fa23ae09
Aleksey Orlov <orlov@farbik.ru>
2015-07-15 01:42:52 +0300

    update

--- js/app.js
+++ js/app.js
@@ -1,1 +1,289 @@
-"use strict";const ItemsList=React.createClass({displayName:"ItemsList",getDataFromServer:function(){return[{id:0,name:"Побег из Шоушенка",year:1994,time:142,director:"Фрэнк Дарабонт",rating:9.214},{id:1,name:"Зеленая миля",year:1999,time:189,director:"Фрэнк Дарабонт",rating:9.167},{id:2,name:"Форрест Гамп",year:1994,time:142,director:"Роберт Земекис",rating:9.019},{id:3,name:"Список Шиндлера",year:1993,time:195,director:"Стивен Спилберг",rating:8.911},{id:4,name:"1+1",year:2011,time:112,director:"Оливье Накаш",rating:8.893},{id:5,name:"Король Лев",year:1994,time:89,director:"Роджер Аллерс",rating:8.786},{id:6,name:"Начало",year:2010,time:148,director:"Кристофер Нолан",rating:8.782},{id:7,name:"Леон",year:1994,time:133,director:"Люк Бессон",rating:8.783},{id:8,name:"Бойцовский клуб",year:1999,time:131,director:"Дэвид Финчер",rating:8.721},{id:9,name:"Жизнь прекрасна",year:1997,time:116,director:"Роберто Бениньи",rating:8.719}]},sorting:function(e,t){if("desc"===this.state.direction){var a=t;t=e,e=a}return+(e.sort>t.sort)||+(e.sort===t.sort)-1},addEvent:function(e){e.addEventListener("click",this.changeSort)},changeSort:function(e){var t,a=e.target.getAttribute("data-sort");a&&(t=e.target.classList.contains("asc")?"desc":"asc",[].forEach.call(document.querySelectorAll("th"),function(e){e.classList.remove("active","asc","desc")}),e.target.classList.add("active",t),this.setState({sort:a,direction:t}))},getInitialState:function(){return{sort:"id",direction:"asc"}},componentDidMount:function(){[].forEach.call(document.querySelectorAll("th"),this.addEvent)},componentDidUpdate:function(){[].forEach.call(document.querySelectorAll("th"),this.addEvent)},render:function(){var e,t,a,r=this.getDataFromServer(),n=this.state.sort,c=this.props.type;r=r.map(function(e){return{sort:e[n],data:e}}),r.sort(this.sorting);var i=r.map(function(e){return React.createElement(Line,{key:e.data.id,data:e.data,type:c})});"big"===c&&(e=React.createElement("th",{"data-sort":"year"},"Премьера"),t=React.createElement("th",{"data-sort":"time"},"Продолжительность"),a=React.createElement("th",{className:"mdl-data-table__cell--non-numeric","data-sort":"director"},"Режиссёр"));var d=React.createElement("table",{className:"mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp"},React.createElement("thead",null,React.createElement("tr",null,React.createElement("th",{className:"mdl-data-table__cell--non-numeric","data-sort":"name"},"Название"),e,t,a,React.createElement("th",{"data-sort":"rating"},"Рейтинг"))),React.createElement("tbody",null,i));return d}}),Line=React.createClass({displayName:"Line",render:function(){var e,t,a;return"big"===this.props.type&&(e=React.createElement("td",null,this.props.data.year," год"),t=React.createElement("td",null,this.props.data.time," мин."),a=React.createElement("td",{className:"mdl-data-table__cell--non-numeric"},this.props.data.director)),React.createElement("tr",null,React.createElement("td",{className:"mdl-data-table__cell--non-numeric"},this.props.data.name),e,t,a,React.createElement("td",null,this.props.data.rating))}});React.render(React.createElement(ItemsList,{type:"min"}),document.querySelector(".page-content"));const Menu=React.createClass({displayName:"Menu",render:function(){var e=[{id:0,text:"Полный список",type:"big"},{id:1,text:"Короткий список",type:"min"}],t=e.map(function(e){return React.createElement(MenuLink,{key:e.id,text:e.text,type:e.type})});return React.createElement("nav",{className:"mdl-navigation"},t)}}),MenuLink=React.createClass({displayName:"MenuLink",tableRender:function(e){var t=e.target.getAttribute("data-type");document.querySelector(".mdl-layout__obfuscator").click(),React.render(React.createElement(ItemsList,{type:t}),document.querySelector(".page-content")),e.preventDefault()},componentDidMount:function(){this.getDOMNode().addEventListener("click",this.tableRender)},render:function(){return React.createElement("a",{className:"mdl-navigation__link","data-type":this.props.type,href:"#"},this.props.text)}});React.render(React.createElement(Menu,null),document.querySelector(".mdl-layout__drawer"));
+'use strict';
+
+/**
+ * Основной компонент формируем таблицу
+ */
+const ItemsList = React.createClass({displayName: "ItemsList",
+
+
+    /**
+     * Получаем данные с сервера
+     * @returns {*[]}
+     */
+    getDataFromServer: function () {
+        return [
+            {
+                "id": 0,
+                "name": "Побег из Шоушенка",
+                "year": 1994,
+                "time": 142,
+                "director": "Фрэнк Дарабонт",
+                "rating": 9.214
+            },
+            {
+                "id": 1,
+                "name": "Зеленая миля",
+                "year": 1999,
+                "time": 189,
+                "director": "Фрэнк Дарабонт",
+                "rating": 9.167
+            },
+            {
+                "id": 2,
+                "name": "Форрест Гамп",
+                "year": 1994,
+                "time": 142,
+                "director": "Роберт Земекис",
+                "rating": 9.019
+            },
+            {
+                "id": 3,
+                "name": "Список Шиндлера",
+                "year": 1993,
+                "time": 195,
+                "director": "Стивен Спилберг",
+                "rating": 8.911
+            },
+            {
+                "id": 4,
+                "name": "1+1",
+                "year": 2011,
+                "time": 112,
+                "director": "Оливье Накаш",
+                "rating": 8.893
+            },
+            {
+                "id": 5,
+                "name": "Король Лев",
+                "year": 1994,
+                "time": 89,
+                "director": "Роджер Аллерс",
+                "rating": 8.786
+            },
+            {
+                "id": 6,
+                "name": "Начало",
+                "year": 2010,
+                "time": 148,
+                "director": "Кристофер Нолан",
+                "rating": 8.782
+            },
+            {
+                "id": 7,
+                "name": "Леон",
+                "year": 1994,
+                "time": 133,
+                "director": "Люк Бессон",
+                "rating": 8.783
+            },
+            {
+                "id": 8,
+                "name": "Бойцовский клуб",
+                "year": 1999,
+                "time": 131,
+                "director": "Дэвид Финчер",
+                "rating": 8.721
+            },
+            {
+                "id": 9,
+                "name": "Жизнь прекрасна",
+                "year": 1997,
+                "time": 116,
+                "director": "Роберто Бениньи",
+                "rating": 8.719
+            }
+        ];
+    },
+
+    /**
+     * Сортировка объектов по свойству sort
+     */
+    sorting: function (obj1, obj2) {
+        if (this.state.direction === 'desc') {
+            var tmp = obj2;
+            obj2 = obj1;
+            obj1 = tmp;
+        }
+        return +(obj1.sort > obj2.sort) || +(obj1.sort === obj2.sort) - 1;
+    },
+
+    /**
+     * Вешаем событие пересортировки
+     */
+    addEvent: function (el) {
+        el.addEventListener('click', this.changeSort);
+    },
+
+    /**
+     * Сортируем по полю указанному в атрибуте data-sort
+     */
+    changeSort: function (e) {
+        var dirClass,
+            sort = e.target.getAttribute('data-sort');
+
+        if (sort) {
+            e.target.classList.contains('asc') ? dirClass = 'desc' : dirClass = 'asc';
+
+            [].forEach.call(document.querySelectorAll('th'), function (el) {
+                el.classList.remove('active', 'asc', 'desc');
+            });
+
+            e.target.classList.add('active', dirClass);
+            this.setState({sort: sort, direction: dirClass});
+        }
+    },
+
+    /**
+     * Это выполняется перед функцией render. Возвращаемый объект присваивается в this.state
+     */
+    getInitialState: function () {
+        return {sort: 'id', direction: 'asc'};
+    },
+
+    /**
+     * вызывается react'ом, когда компонент был отрисован на странице
+     */
+    componentDidMount: function () {
+
+        /**
+         * Перебераем все th таблицы и вешаем на них события
+         */
+        [].forEach.call(document.querySelectorAll('th'), this.addEvent);
+    },
+
+    /**
+     * вызывается react'ом, когда компонент был перерисован
+     */
+    componentDidUpdate: function () {
+        [].forEach.call(document.querySelectorAll('th'), this.addEvent);
+    },
+
+    /**
+     * Отображение
+     */
+    render: function () {
+        var year, time, director, data = this.getDataFromServer();
+        var sort = this.state.sort;
+        var type = this.props.type;
+
+        /**
+         * Формируем массив с учётом сортировки по полю field
+         */
+        data = data.map(function (e) {
+            return {
+                sort: e[sort],
+                data: e
+            };
+        });
+
+        /**
+         * Сортируем объекты
+         */
+        data.sort(this.sorting);
+
+        /**
+         * Получаем список строк на основе данных
+         */
+        var lines = data.map(function (line) {
+            return React.createElement(Line, {key: line.data.id, data: line.data, type: type})
+        });
+
+        if (type === 'big') {
+            year = React.createElement("th", {"data-sort": "year"}, "Премьера");
+            time = React.createElement("th", {"data-sort": "time"}, "Продолжительность");
+            director = React.createElement("th", {className: "mdl-data-table__cell--non-numeric", "data-sort": "director"}, "Режиссёр");
+        }
+
+        var table = React.createElement("table", {className: "mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp"}, 
+            React.createElement("thead", null, 
+            React.createElement("tr", null, 
+                React.createElement("th", {className: "mdl-data-table__cell--non-numeric", "data-sort": "name"}, "Название"), 
+                year, 
+                time, 
+                director, 
+                React.createElement("th", {"data-sort": "rating"}, "Рейтинг")
+            )
+            ), 
+            React.createElement("tbody", null, lines)
+        );
+
+        return table;
+    }
+});
+
+/**
+ * Компонент формирующий строку таблицы
+ * this.props.data: переданные в компонент данные
+ */
+const Line = React.createClass({displayName: "Line",
+    render: function () {
+        var year, time, director;
+
+        if (this.props.type === 'big') {
+            year = React.createElement("td", null, this.props.data.year, " год");
+            time = React.createElement("td", null, this.props.data.time, " мин.");
+            director = React.createElement("td", {className: "mdl-data-table__cell--non-numeric"}, this.props.data.director);
+        }
+
+        return React.createElement("tr", null, 
+            React.createElement("td", {className: "mdl-data-table__cell--non-numeric"}, this.props.data.name), 
+            year, 
+            time, 
+            director, 
+            React.createElement("td", null, this.props.data.rating)
+        );
+    }
+});
+
+/**
+ * Рендер таблицы на страницу
+ */
+React.render(
+    React.createElement(ItemsList, {type: "min"}),
+    document.querySelector('.page-content')
+);
+
+
+/**
+ *
+ */
+const Menu = React.createClass({displayName: "Menu",
+    render: function () {
+        var data = [
+            {id: 0, text: 'Полный список', type: 'big'},
+            {id: 1, text: 'Короткий список', type: 'min'}
+        ];
+
+        var items = data.map(function (item) {
+            return React.createElement(MenuLink, {key: item.id, text: item.text, type: item.type})
+        });
+
+        return React.createElement("nav", {className: "mdl-navigation"}, 
+            items
+        )
+    }
+});
+
+const MenuLink = React.createClass({displayName: "MenuLink",
+
+    tableRender: function (e) {
+        var type = e.target.getAttribute('data-type');
+        document.querySelector('.mdl-layout__obfuscator').click();
+        React.render(
+            React.createElement(ItemsList, {type: type}),
+            document.querySelector('.page-content')
+        );
+        e.preventDefault();
+    },
+    componentDidMount: function () {
+        this.getDOMNode().addEventListener('click', this.tableRender);
+    },
+    render: function () {
+        return React.createElement("a", {className: "mdl-navigation__link", "data-type": this.props.type, href: "#"}, this.props.text);
+    }
+});
+
+React.render(
+    React.createElement(Menu, null),
+    document.querySelector('.mdl-layout__drawer')
+);
